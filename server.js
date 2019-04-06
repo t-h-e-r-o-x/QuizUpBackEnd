e83853539f37d9e45db52e35752f963a98cb00b9
t-h-e-r-o-x <31180828+t-h-e-r-o-x@users.noreply.github.com>
2019-04-06 14:24:08 +0530

    AuthSetup

--- server.js
+++ server.js
@@ -6,13 +6,72 @@ const bcrypt = require('bcryptjs');
 
 const app = express();
 
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'quizup'
+  }
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/signin', (req,res) => {
-
+  const { email, password } = req.body;
+  if(!email || !password){
+    return res.status(400).json('Incorrect form submission');
+  }
+  db.select('email','hash').from('login')
+  .where('email','=', email)
+  .then( data => {
+    const isValid = bcrypt.compareSync(password, data[0].hash);
+    if(isValid){
+      return db.select('*').from('users')
+        .where('email','=',email)
+        .then(user => {
+          res.json(user[0]);
+        })
+        .catch( err => res.status(400).json('Unable to get user'))
+    }else{
+      res.status(400).json('Wrong credentials');
+    }
+  })
+  .catch(err => res.status(400).json('Wrong credentials'))
+}
 })
 
 app.post('/register', (req,res) => {
-
+  const { email, name, password } = req.body;
+  if(!email || !password || !name){
+    return res.status(400).json('Incorrect form submission');
+  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+      .returning('*')
+      .insert({
+        email: loginEmail[0],
+        name: name,
+        joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+    .catch(err => res.status(400).json('Unable to register'));
 })
+
+app.listen(3000);
